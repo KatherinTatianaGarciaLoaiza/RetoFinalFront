586a2ce019bae75889fe2305b036bf937227f716
MiguelRamirez97 <73442213+MiguelRamirez97@users.noreply.github.com>
2021-06-06 15:14:39 -0500

    Login y Logout funcionales

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,33 +1,18 @@
 import React from 'react'
-import { useAuthState } from "react-firebase-hooks/auth";
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-  } from 'react-router-dom'
-
-  import firebase from "firebase/app";
-  import "firebase/firestore";
-  import "firebase/auth";
-
-  firebase.initializeApp({
-    apiKey: "AIzaSyCZ3227IZBEdOlaf_1bGxnJUBUaJiLSwH8",
-    authDomain: "despliegueretofinal.firebaseapp.com",
-    projectId: "despliegueretofinal",
-    storageBucket: "despliegueretofinal.appspot.com",
-    messagingSenderId: "957933930402",
-    appId: "1:957933930402:web:0984f2015e9865858014fb",
-    measurementId: "G-90ZG9ST9LW"
-  });
-  
-  const auth = firebase.auth();
+import { Container, Row } from 'react-bootstrap';
+import { SignIn, SignOut } from '../App'
 
 export default function LandingPage() {
     return (
         <div>
-            
+          <Container>
+            <Row>
+              <p>Login</p>
+              <SignIn/>
+              <SignOut/>
+            </Row>
+          </Container>
         </div>
     )
 }
+
