e7ad8c1ec3cad77f7440c7343e0008a91dd3e120
Tatiana Loaiza <ktatiana.garcia@udea.edu.co>
2021-06-05 19:29:28 -0500

    Redirecciones

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import {
 import ConfigurationPage from './pages/ConfigurationPage';
 import HomePage from "./pages/HomePage";
 import AllOKRSPage from "./pages/AllOKRSPage";
-import MyOKRS from "./pages/MyOKRS";
-import UserOKRS from "./pages/UserOKRS";
+import MyOKRSPage from "./pages/MyOKRSPage";
+import UserOKRSPage from "./pages/UserOKRSPage";
 import ProfilePage from "./pages/ProfilePage";
-import CreateOKR from "./pages/CreateOKR";
+import CreateOKRPage from "./pages/CreateOKRPage";
 
 function App() {
 
@@ -22,11 +22,11 @@ function App() {
           <Route exact path="/" component={() => {
             return <HomePage/>}} />
             <Route exact path="/AllOKRS" component={AllOKRSPage}/>
-            <Route exact path="/MyOKRS" component={MyOKRS}/>
+            <Route exact path="/MyOKRS" component={MyOKRSPage}/>
             <Route exact path="/Home" component={HomePage}/>
-            <Route exact path="/UserOKRS" component={UserOKRS}/>
+            <Route exact path="/UserOKRS" component={UserOKRSPage}/>
             <Route exact path="/ProfileUser" component={ProfilePage}/>
-            <Route exact path="/CreateOKR" component={CreateOKR}/>
+            <Route exact path="/CreateOKR" component={CreateOKRPage}/>
             <Route exact path="/ConfigurationNotifications" component={ConfigurationPage}/>
         </Switch> 
   </Router>   
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import List from '@material-ui/core/List';
 import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Link } from 'react-router-dom'
-import {estilos} from './DesignNaSi';
+import { estilos } from './DesignNaSi';
 
 export default function Sidebar({ texto, ruta }) {
   const classes = estilos();
@@ -28,21 +24,13 @@ export default function Sidebar({ texto, ruta }) {
         </List>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <p>Dashboard</p>
+          <ListItem> <Link to={`/AllOKRS`} className="button">Todos los OKR</Link> </ListItem>
+          <ListItem> <Link to={`/UserOKRS`} className="button">Pepito Perez y vertical</Link> </ListItem>
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem> <Link to={`/ConfigurationNotifications`} className="button">Configuracion de notificaciones</Link> </ListItem>
         </List>
       </div>
     </Drawer>
