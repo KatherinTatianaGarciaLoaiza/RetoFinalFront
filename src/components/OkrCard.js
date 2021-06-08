43885c7598c64d44dd9704f4fb8157ed28ac8dbe
Stewar <stewar1020@gmail.com>
2021-06-08 16:29:27 -0500

    Adding components Card & OKRCard

---
+++ src/components/OkrCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import KrCard from './Card';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const OkrCard = ({ okr }) => {
+  return (
+    <div>
+      <div>
+        <h2>{okr.title}</h2>
+        <button>
+          <EditIcon />
+        </button>
+        <button>
+          <DeleteIcon />
+        </button>
+      </div>
+      {okr.krs.map((kr) => (
+        <KrCard
+          key={kr.id}
+          keyResult={kr.keyResult}
+          description={kr.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default OkrCard;
