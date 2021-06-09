5869331083b872d4175058acd90c6d312a3ae98e
Tatiana Loaiza <ktatiana.garcia@udea.edu.co>
2021-06-09 10:52:17 -0500

    mejora de apariencia en mensaje de bienvenida

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -3,40 +3,42 @@ import Logo from '../images/Logo.png';
 import { auth } from './Logging';
 import { nombre } from './Avatar'
 import { Link } from 'react-router-dom';
-import "../styles/style.css"
-import { Modal, Button } from 'react-bootstrap'
+import "../styles/style.css";
+import { Modal, Button } from 'react-bootstrap';
 
-function Example() {
+function WelcomeMessage() {
+
     const [show, setShow] = useState(true);
 
     const handleClose = () => setShow(false);
 
     return (
         <>
-            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} >
-                <Modal.Header closeButton>
+            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} style={{ border: '2px solid #F0950E' }}>
+                <Modal.Header closeButton style={{ border: '2px solid #F0950E' }}>
                     <Modal.Title>
                         <div style={{ display: "flex", justifyContent: "center" }}>
                             <img src={Logo} style={{ width: "80%", height: "30%" }}></img>
                         </div>
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <h3 className="title3">!!Te da la bienvenida {nombre(auth.currentUser.displayName)}, 
-                    Estamos felices de tenerte de nuevo con notros!!</h3>
+                <Modal.Body >
+                    <h3 className="title3">!!Te da la bienvenida {nombre(auth.currentUser.displayName)},
+                        Estamos felices de tenerte de nuevo con notros!!</h3>
                     <br />
                     <h5 className="title3"> Esperamos poder aprender mucho de tus aportes. </h5>
                     <br />
                 </Modal.Body>
-                <div style={{ display: "flex", justifyContent: "center" }}>
+                <div style={{ display: "flex", justifyContent: "center" }} >
                     <Link to={`/Home`} className="button">
                         <Button variant="contained" style={{ background: "#F0950E", color: "#ffffff" }} >
                             Aceptar </Button></Link>
                 </div>
                 <br />
             </Modal>
+
         </>
     );
 }
 
-export default Example;
+export default WelcomeMessage;
