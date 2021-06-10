2eaef7a97afa838b48e18d5a953190f70bd7aab6
Stewar <stewar1020@gmail.com>
2021-06-10 12:16:18 -0500

    Reset global state with brand in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,19 @@ import { Link } from 'react-router-dom';
 import { SignOut } from './Logging';
 import { auth } from './Logging';
 import AvatarUser from './Avatar';
+import { connect } from 'react-redux';
+import { postokr } from '../actions/okrActions';
 
-export default function NavbarSofKa({ classes }) {
+function NavbarSofKa({ dispatch, classes }) {
     const { displayName, photoURL } = auth.currentUser || { uid: "", displayName: "", photoURL: "", email: "" }
+    const onclick = () =>{
+        dispatch(postokr())
+    }
     return (
         <AppBar position="fixed" className={classes.appBar} style={{ background: "#F0950E" }} >
             <div className="col-md">
                 <Navbar collapseOnSelect expand="lg" variant="dark">
-                    <Navbar.Brand ><Link to="/Home" style={{ color: "#ffffff" }} ><h1 className="title">SOKFA OKR</h1></Link> </Navbar.Brand>
+                    <Navbar.Brand ><Link to="/Home" onClick={onclick} style={{ color: "#ffffff" }} ><h1 className="title">SOKFA OKR</h1></Link> </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
@@ -30,3 +35,6 @@ export default function NavbarSofKa({ classes }) {
         </AppBar>
     )
 }
+
+
+export default connect()(NavbarSofKa);
