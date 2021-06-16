7b3ace3ddb75f44bf20e9d6f00f1625c15adb251
alejo <cavz11397@gmail.com>
2021-06-16 15:17:27 -0500

    obtener valor de la promise

--- src/components/notifications/ModalBell.js
+++ src/components/notifications/ModalBell.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import { Modal } from '@material-ui/core';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+import { URI } from '../../actions/okrActions';
+import { auth } from '../logging/Logging';
 
 function ModalBell() {
 
+    const [estado, setEstado] = useState("message");
     const [modal, setModal] = useState(false);
     const open_close_Modal = () => {
         setModal(!modal);
@@ -17,13 +21,25 @@ function ModalBell() {
                     <strong>Notificaciones</strong>
                     <button onClick={() => open_close_Modal()}>X</button>
                 </header>
+                <body>
+                </body>
             </div>
         </div>
     );
 
+    function Respuesta() {
+        return axios.get(`${URI}/GetNotifications/${auth.currentUser.email}`)
+        .then((value) => setEstado(value.data) );
+    }
+
     return (
         <div className="App" id="App">
-            <NotificationsIcon style={{ color: 'white', cursor: "pointer" }} fontSize="large" onClick={() => open_close_Modal()} ><FiberManualRecordIcon/></NotificationsIcon>
+            <NotificationsIcon style={{ color: 'white', cursor: "pointer" }} fontSize="large" onClick={() => {
+                Respuesta()
+                console.log("estado "+ estado[1].message)
+                open_close_Modal()
+            }
+            } />
             <Modal open={modal} onClose={open_close_Modal}>
                 {body}
             </Modal>
