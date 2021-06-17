c07d56e273f9d17ccc5728d34d3df2ba3332572d
Brian olarte <brayan_andrey23@hotmail.com>
2021-06-16 20:05:51 -0500

    [FEAT] formando el objeto para enviar actualizacion de porcentages al backend

--- src/pages/ReadjustPercentage.js
+++ src/pages/ReadjustPercentage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import NavbarSofKa from '../components/structure/Navbar';
 import { estilos } from '../components/structure/DesignNaSi';
-import OKRUpdateWeigth from '../components/planning/OKRUpdateWeigth';
+import FormUpdateWeigthKRS from '../components/planning/FormUpdateWeigthKRS';
 
 
 const ReadjustPercentage = () => {
@@ -12,7 +12,7 @@ const ReadjustPercentage = () => {
       <NavbarSofKa classes={classes} />
       <main className={classes.content}>
         <Toolbar />
-        <OKRUpdateWeigth/>
+        <FormUpdateWeigthKRS />
       </main>
     </div>
   );
